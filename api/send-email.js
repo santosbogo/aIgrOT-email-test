9dc3453811a6e5e8d9356b155af60eb0d21363e1
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2026-01-28 07:45:53 -0400

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -1,28 +1,10 @@
-import { Resend } from "resend";
+import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_D4yZdtyE_5YHEfmZEK7x1MuoXGWJHzr9D');
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Only POST allowed" });
-  }
-
-  try {
-    const { name, email, message } = req.body || {};
-
-    await resend.emails.send({
-      from: "Formulario <onboarding@resend.dev>",
-      to: "TU_CORREO@gmail.com",
-      subject: "Nuevo mensaje",
-      html: `
-        <p><b>Nombre:</b> ${name}</p>
-        <p><b>Email:</b> ${email}</p>
-        <p><b>Mensaje:</b> ${message}</p>
-      `,
-    });
-
-    return res.status(200).json({ ok: true });
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
-}
+resend.emails.send({
+  from: 'onboarding@resend.dev',
+  to: 'santosbogo@gmail.com',
+  subject: 'Hello World',
+  html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
+});
